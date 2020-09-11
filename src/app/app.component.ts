0519af0d08f67512ddc4ebea567b887e8dac1b2a
tsteelematc <tsteele@madisoncollege.edu>
2020-09-11 16:55:48 -0500

    Our #week-02 Thu in-class changes...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'quiz-editor';
+  // title = `quiz-editor${100 + 23}`;
+  title = 'Quiz Editor';
+
+  quizzes = [
+    "Quiz 1"
+    , "Quiz 2"
+    , `Quiz ${1 + 2}`
+    , `Quiz ${Math.max(4, -12234)}`
+  ];
+
+  addFunnyQuiz = () => {
+    this.quizzes = [
+      ...this.quizzes
+      , "Funny Quiz"
+    ];
+  };
 }
